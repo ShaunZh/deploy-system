e77c1293c2fdf3621580d40cf3e7b2a4c1b5e6a0
shaunZh <308826842@qq.com>
2021-11-18 23:21:43 +0800

    add: antd

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,14 +3,17 @@
  * @Author: Hexon
  * @Date: 2021-08-30 10:00:50
  * @LastEditors: Hexon
- * @LastEditTime: 2021-08-31 13:42:58
+ * @LastEditTime: 2021-11-18 23:12:09
  */
 import React from 'react';
+import { Button } from 'antd';
 
 
 export default function Home(): React.ReactElement {
   return (
-    <p>Home</p>
+    <p>
+      <Button>antd button</Button>
+    </p>
   );
 }
 
