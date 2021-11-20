f87a65c260e60941a9c135158be5845e8f821a99
shaunZh <308826842@qq.com>
2021-11-20 22:35:58 +0800

    add: 添加ErrorBoundary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import './style';
 import RouteConfig from './router';
 import { HashRouter as Router } from 'react-router-dom';
 import Layout from '@/components/Layout/Base';
+import ErrorBoundary from '@/components/ErrorBoundary';
 import { Spin } from 'antd';
 
 export default function App(): React.ReactElement {
   return (
     <Router>
       <Layout>
-        <Suspense fallback={<Spin />} >
-          <RouteConfig></RouteConfig>
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<Spin />} >
+            <RouteConfig></RouteConfig>
+          </Suspense>
+        </ErrorBoundary>
       </Layout>
     </Router>
   );
