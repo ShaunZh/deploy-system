fd81e19d32e819ca1c1c6ec74705e3c0f5538866
zhangjie <hexon804@163.com>
2021-12-03 18:04:37 +0800

    update: pipeline api

--- frontend/src/services/pipeline.ts
+++ frontend/src/services/pipeline.ts
@@ -2,14 +2,14 @@
  * @Description:
  * @Author: Hexon
  * @Date: 2021-11-27 22:40:33
- * @LastEditors: Hexon
- * @LastEditTime: 2021-11-27 22:53:25
+ * @LastEditors: Zhang jie
+ * @LastEditTime: 2021-12-03 09:44:40
  */
 import request from './request';
 
 export const pipeline_list = (args: any): Promise<any> => request.post('/pipeline/list', args);
 export const pipeline_template_list = (): Promise<any> => request.post('/pipeline/template/list');
-export const pipeline_detail = (args: { id: string }): Promise<any> => request.post('/pipeline/detail', args);
+export const pipeline_detail = (args: { id: string }): PromiseResponse<PipelineDetail> => request.post('/pipeline/detail', args);
 
 export default {
   pipeline_list,
