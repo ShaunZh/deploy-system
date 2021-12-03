75dbdf97ace95e0208b7ca97d44a3706eb514c11
zhangjie <hexon804@163.com>
2021-12-03 18:04:49 +0800

    add: types

--- frontend/src/@types/assets/base.d.ts
+++ frontend/src/@types/assets/base.d.ts
@@ -2,9 +2,14 @@
  * @Description:
  * @Author: Hexon
  * @Date: 2021-11-29 22:42:33
- * @LastEditors: Hexon
- * @LastEditTime: 2021-11-29 22:42:34
+ * @LastEditors: Zhang jie
+ * @LastEditTime: 2021-12-03 18:04:00
  */
 interface IObject {
   [index: string]: string
 }
+
+type PromiseResponse<DATA> = Promise<{
+  data: DATA;
+  success: boolean
+}>;
